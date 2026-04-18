d21963326e4426480a913c87c1aaa48a99b78e95
Raptor <karl.b.albrecht@gmail.com>
2026-04-18 11:16:53 +0200

    add solution for pascal's triangle
    Implement Pascal's triangle generation algorithm.

---
+++ 01-easy/0118-pascals-triangle/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        triangle = [[1] * (n + 1) for n in range(numRows)]
+        for i in range(2, numRows):
+            for j in range(1, i):
+                triangle[i][j] = triangle[i - 1][j] + triangle[i - 1][j - 1]
+        return triangle
